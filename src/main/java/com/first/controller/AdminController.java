83b1c4dcac0e8a7336779c6e2c9fe7901bd10bf9
pjh <hateme18@naver.com>
2023-09-19 17:25:23 +0900

    상품관리/카테고리 리스트

--- src/main/java/com/first/controller/AdminController.java
+++ src/main/java/com/first/controller/AdminController.java
@@ -1,13 +1,17 @@
 package com.first.controller;
 
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.first.domain.Criteria;
 import com.first.domain.DrinkVO;
 import com.first.service.AdminService;
 
@@ -27,9 +31,27 @@ import com.first.service.AdminService;
 	    @RequestMapping(value="main", method = RequestMethod.GET)
 	    public void adminMainGET() throws Exception{
 	        
-	        logger.info("관리자 페이지로 이동");
-	        
-	      
-	        
+	        logger.info("관리자 페이지로 이동");	        	        
 	    }
+	    
+	    //상품 관리(상품목록) 페이지 접속
+		@RequestMapping(value = "goodsManage", method = RequestMethod.GET)
+		public void goodsManageGET(Criteria cri, Model model) throws Exception{
+	
+			logger.info("상품 관리(상품 목록) 페이지 접속");
+		}
+	    
+
+		
+	  
+	  //상품 등록 페이지 접속
+	  @RequestMapping(value = "goodsEnroll", method = RequestMethod.GET)
+	  public void goodsEnrollGET() throws Exception{
+		  logger.info("상품 등록 페이지 접속");
+	  }
+	  
+	  
+	  List list = adminService.cateList();
+	  
+	  
 }
--- src/main/java/com/first/service/AdminService.java
+++ src/main/java/com/first/service/AdminService.java
@@ -1,9 +1,16 @@
 package com.first.service;
 
+import java.util.List;
+
+import com.first.domain.CateVO;
+import com.first.domain.Criteria;
 import com.first.domain.DrinkVO;
 
 public interface AdminService {
 
-	/* 주류 등록 */
-	public void drinkEnroll(DrinkVO drink);
+
+	public void drinkEnroll(DrinkVO drink); //주류 등록
+	public List<CateVO> cateList(); //카테고리 리스트
+	public List<DrinkVO> goodsGetList(Criteria cri); //상품 리스트
+	public int goodsGetTotal(Criteria cri);	 //상품 총 개수
 }
