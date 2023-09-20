cb3149a126ddcbc1106652bbe51a2ad2db8e17d1
indramj <144644759+indramj@users.noreply.github.com>
2023-09-20 13:55:11 +0900

    Revert "Feature/3"

--- src/main/java/com/first/mapper/AdminMapper.java
+++ src/main/java/com/first/mapper/AdminMapper.java
@@ -1,16 +1,8 @@
 package com.first.mapper;
 
-import java.util.List;
-
-import com.first.domain.CateVO;
-import com.first.domain.Criteria;
 import com.first.domain.DrinkVO;
 
 public interface AdminMapper {
 
-	public void drinkEnroll(DrinkVO drink); //주류 등록
-	public List<CateVO> cateList(); //카테고리 리스트
-	public List<DrinkVO> goodsGetList(Criteria cri); //상품 리스트
-	public int goodsGetTotal(Criteria cri); //상품 총 개수
-	
+	public void drinkEnroll(DrinkVO drink);
 }
