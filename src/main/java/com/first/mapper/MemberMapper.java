6077d6283c6a715a0df2c935e3792b4e6bce142b
lsh <byhosino@gmail.com>
2023-09-25 18:39:20 +0900

    MemberMapper 재생 완료

---
+++ src/main/java/com/first/mapper/MemberMapper.java
@@ -0,0 +1,13 @@
+package com.first.mapper;
+
+import com.first.domain.MemberVO;
+
+public interface MemberMapper {
+	
+	public void joinMember(MemberVO memberVO);
+	
+	public int checkId(String memberId);
+	
+	public MemberVO loginMember(MemberVO member);
+
+}
